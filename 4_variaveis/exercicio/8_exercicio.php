6aa0132c4ae034ccdaf1e8913a1775fb1f4e03e8
Vinicius <vinintc01@gmail.com>
2023-09-07 11:42:51 -0300

    adicionando exercicio 8

---
+++ 4_variaveis/exercicio/8_exercicio.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = 'Vinícius';
+
+$idade = 19;
+
+$altura = 1.7;
+
+$sonhos = ['policial', 'militar', 'programação'];
+
+echo "Meu nome é $nome, tenho $idade anos, e possuo $altura metros, e tenho o sonho de ser $sonhos[0]";
+
+echo '<br>';
+
+// print_r($sonhos[0]);
+
+?>
